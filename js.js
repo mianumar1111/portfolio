4a720b08a1289941b0ab12e1fbcf1dcaf1a4ee8d
mianumar1111 <144631593+mianumar1111@users.noreply.github.com>
2023-12-05 03:17:17 +0500

    again updated

--- js.js
+++ js.js
@@ -1,6 +1,13 @@
 var body = document.body
-// console.log(img.getAttribute('src'))
+var navA=document.querySelectorAll('.left  li a')
+var nav=document.querySelector('nav')
+console.log(nav)
+
+
 
 themeimg.addEventListener('click',function(){
     body.classList.toggle('bg-color')
+    nav.classList.toggle('nav2')
+    nav.classList.toggle('navB')
+    
 })
