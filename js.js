e2e3d41c06cfe055bac856222f86ece576f0b0fc
mianumar1111 <144631593+mianumar1111@users.noreply.github.com>
2023-12-08 03:23:27 +0500

    updated

--- js.js
+++ js.js
@@ -2,6 +2,7 @@ var body = document.body
 var navA=document.querySelectorAll('.left  li a')
 var nav=document.querySelector('nav')
 var themeimg = document.querySelector('#themeimg')
+var progress = document.querySelector('.progress')
 // console.log(themeimg)
 
 
@@ -10,6 +11,8 @@ themeimg.addEventListener('click',function(){
     body.classList.toggle('bg-color')
     nav.classList.toggle('nav2')
     nav.classList.toggle('navB')
+    progress.classList.toggle('progress')
+    progress.classList.toggle('progress-dark')
     
     toggle = !toggle
     if(toggle){
@@ -17,5 +20,14 @@ themeimg.addEventListener('click',function(){
     }else{
         themeimg.src='day.png'
     }
-    // console.log(toggle)
 })
+
+function updateWidthOnScroll() {
+    var scrollPosition = window.scrollY;
+    var newWidth = (scrollPosition / (document.body.scrollHeight - window.innerHeight)) * 100;
+    progress.style.width = newWidth + '%';
+}
+
+window.onscroll = function() {
+    updateWidthOnScroll();
+};
